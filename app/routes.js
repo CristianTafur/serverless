f8b800ea69afaea91f34db3700492f535bc28c7f
Cristian Tafur <cristian.tafur@atlanticsoft.us>
2020-11-16 17:18:48 -0500

    add logs

--- app/routes.js
+++ app/routes.js
@@ -1,10 +1,11 @@
-const { Router } = require('express');
+const { Router, json } = require('express');
 const app = Router();
-
+const logger = require('./utils/logs'); 
 const usersControllers = require('../app/controllers/usersControllers');
 
 app.get('/', async function (req, res) {
     res.send({message:'Hello World!'});
+    logger.create('' + JSON.stringify({message:'Hello World!'}));
 })
 
 app.post('/user',usersControllers.create);

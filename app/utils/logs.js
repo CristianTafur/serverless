23e387f40916f7ffc3fe3e13fb19440c08b40e14
cristian tafur <cristian.tafur@atlanticsoft.us>
2020-11-16 23:59:12 -0500

    add util logs

--- app/utils/logs.js
+++ app/utils/logs.js
@@ -1,10 +1,10 @@
 const logs = module.exports;
 const fs = require('fs');
 
-logs.create = (log) => new Promise((resolve,reject)=>{
+logs.create = (log, error = false ) => new Promise((resolve,reject)=>{
     const date = new Date();
     const getMonth = ""+date.getMonth();
-    fs.writeFile("./app/logs/log.txt", '[ log : '+date.toLocaleDateString()+ ' ] '+log, function(err) {
+    fs.appendFile("./app/logs/logs-"+date.toLocaleDateString()+".txt", '[ '+(error?'error':'log') + ' | '+date.toLocaleTimeString()+' ] '+log + '\n', function(err) {
         if(err) {
             return reject(err);
         }

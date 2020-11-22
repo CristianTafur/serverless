0b07b9c64b9a933726d9055bc89a43930e2a5db4
Cristian Camilo tafur morales <cristian.tafur@atlanticsoft.us>
2020-11-22 12:01:19 -0500

    refactor gitlab-ci

--- app.js
+++ app.js
@@ -11,5 +11,19 @@ app.set('port', 3000);
 app.listen(app.get('port'), async () => {
   console.log('SERVER ON PORT ', app.get('port'));
   await DB.migrate.latest();
+  const {
+    DB_NAME,
+    DB_USER,
+    DB_PASS,
+    DB_HOST,
+    DB_PORT,
+  } = process.env;
+  console.log({
+    DB_NAME,
+    DB_USER,
+    DB_PASS,
+    DB_HOST,
+    DB_PORT,
+  });
 });
 module.exports = app;

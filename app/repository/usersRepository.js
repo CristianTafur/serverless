ad477b96d72372ff1384ce48d5191e958dcec7f1
Cristian Camilo tafur morales <cristian.tafur@atlanticsoft.us>
2020-11-23 17:20:42 -0500

    try fix  deploy env

--- app/repository/usersRepository.js
+++ app/repository/usersRepository.js
@@ -1,8 +1,7 @@
 const usersRepository = module.exports;
 
 const DB = require('../utils/DataBase');
-const config = require('../config/DataBase');
 
 usersRepository.create = async (user) => DB('users').insert(user).returning('*');
 
-usersRepository.findAll = async () => config;
+usersRepository.findAll = async () => DB('users').select('*');
